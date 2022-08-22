f0c35312be90f6a71126d0a2233b6aa6f706d80c
shashi <shashidharngg@gmail.com>
2022-08-22 23:44:34 +0530

    added:Maxmium String

--- src/com/bridgelabz/genericsmaximum/MaxmiumString.java
+++ src/com/bridgelabz/genericsmaximum/MaxmiumString.java
@@ -1,39 +1,39 @@
 package com.bridgelabz.genericsmaximum;
 
 public class MaxmiumString {
-	public static String getMax(String first,String second,String third) {
+	public static String getMax(String first, String second, String third) {
 		String max;
-		if(first.compareTo(second)>0 && first.compareTo(third)>0) {
-			max=first;
-		} else if(second.compareTo(first)>0 && second.compareTo(third)>0) {
-			max=second;
+		if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+			max = first;
+		} else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+			max = second;
 		} else {
-			max=third;
+			max = third;
 		}
 		return max;
 	}
-	public static float getMax(Float first,Float second,Float third) {
+
+	public static float getMax(Float first, Float second, Float third) {
 		float max;
-		if(first.compareTo(second)>0 && first.compareTo(third)>0) {
-			max=first;
-		} else if(second.compareTo(first)>0 && second.compareTo(third)>0) {
-			max=second;
+		if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+			max = first;
+		} else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+			max = second;
 		} else {
-			max=third;
+			max = third;
 		}
 		return max;
 	}
-	public static int getMax(Integer first,Integer second,Integer third) {
+
+	public static int getMax(Integer first, Integer second, Integer third) {
 		int max;
-		if(first.compareTo(second)>0 && first.compareTo(third)>0) {
-			max=first;
-		} else if(second.compareTo(first)>0 && second.compareTo(third)>0) {
-			max=second;
+		if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+			max = first;
+		} else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+			max = second;
 		} else {
-			max=third;
+			max = third;
 		}
 		return max;
 	}
 }
-
-
