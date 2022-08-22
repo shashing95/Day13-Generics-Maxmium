08712921a120ebaaa0ef8728bac8ffed96148d72
shashi <shashidharngg@gmail.com>
2022-08-22 22:20:23 +0530

    added:Max Number at

---
+++ test/com/bridgelabz/test/MaximumTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.bridgelabz.genericsmaximum.Maximum;
+
+class MaximumTest {
+
+	@Test
+	public void firstMaxTestValidator() {
+		assertEquals(30,Maximum.findMaxInThree(30,20,10));
+	}
+}
